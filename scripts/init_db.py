70ef4589a75e7ef85dbfc70a6dd45cf58bbd7843
Andrew Stocker <amstocker@dons.usfca.edu>
2016-02-29 01:58:48 -0800

    reorg

--- scripts/init_db.py
+++ scripts/init_db.py
@@ -1,10 +1,8 @@
-from sys import argv
-from os import path
 from glob import glob
 from subprocess import check_output, STDOUT
 
 
-for script in glob(path.join(argv[1], "create_*.sql")):
+for script in glob("create_*.sql"):
     
     print(check_output(
         "psql -f {}".format(script),
