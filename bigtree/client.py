8d0737df07f376fed2c495358746adb29c6bee8d
Andrew Stocker <amstocker@dons.usfca.edu>
2016-02-29 01:54:38 -0800

    even more fun

--- bigtree/client.py
+++ bigtree/client.py
@@ -3,12 +3,12 @@
 class Client:
   
   def __init__(self, app, req, ws):
-    self._app = app
+    self._db = app['db']
     self._req = req
     self._ws = ws
   
   async def handle(self, msg):
-    ret = str(await self._app['db'].execute("SELECT 1;"))
+    ret = str(await self._db.execute("SELECT 1;"))
     self._ws.send_str(ret)
   
   async def close(self):
