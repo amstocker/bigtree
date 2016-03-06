855a5c1e64080ed63cb7d9b41fd35a104abdaa63
Andrew Stocker <amstocker@dons.usfca.edu>
2016-03-05 18:03:34 -0800

    minor

--- bigtree/db.py
+++ bigtree/db.py
@@ -3,6 +3,13 @@ import aiopg
 
 
 class Database:
+
+    ## Queries
+    GET_TOP_QUERY = "SELECT post FROM posts ORDER BY score(post) LIMIT %s;"
+    GET_TOP_LIMIT = 50
+    GET_SUBTREE_QUERY = "SELECT post FROM subtree(%s, %s);"
+    GET_SUBTREE_MAX_DEPTH = 5
+
     def __init__(self, dsn):
         self._dsn = dsn
         self._pool = None
@@ -14,7 +21,7 @@ class Database:
         async with self._pool.acquire() as conn:
             async with conn.cursor() as cur:
                 await cur.execute(cmd, args)
-                return (await cur.fetchall())
+                return await cur.fetchall()
     
     async def add_message(self, msg):
         pass
@@ -23,7 +30,10 @@ class Database:
         pass
 
     async def get_top_page(self):
-        pass
+        """
+        Get top N threads.
+        """
+        return await self.execute(GET_TOP_QUERY, GET_TOP_LIMIT)
 
     async def spawn_updater(self, child_uuid):
         pass
