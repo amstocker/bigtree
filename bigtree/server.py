94ae80ccfe7a7fa0ee011e8e36fdb9b817658a32
Andrew Stocker <amstocker@dons.usfca.edu>
2016-03-21 15:16:39 -0700

    rdfffdsafsd

--- bigtree/server.py
+++ bigtree/server.py
@@ -9,7 +9,7 @@ from .pubsub import PubSub
 class Server(web.Application):
 
     async def init(self):
-        self.router.add_route('GET', '/ws/echo', self.websocket_handler())
+        self.router.add_route('GET', '/ws/echo', self.websocket_handler)
 
         self.database = Database(open('private/pg_dsn').read())
         self.pubsub = PubSub('localhost', 6379)
@@ -17,21 +17,16 @@ class Server(web.Application):
         await self.database.connect()
         await self.pubsub.connect()
 
-    def websocket_handler(self):
-        async def handler(req):
-            ws = web.WebSocketResponse()
-            await ws.prepare(req)
+    async def websocket_handler(self, req):
+        ws = web.WebSocketResponse()
+        await ws.prepare(req)
 
-            client = self.new_client(ws)
-            async for msg in ws:
-                if msg.tp == web.MsgType.close:
-                    await client.close(msg)
-                    break
-                else:
-                    await client.handle(msg)
+        client = Client(self, ws)
+        async for msg in ws:
+            if msg.tp == web.MsgType.close:
+                await client.close(msg)
+                break
+            else:
+                await client.handle(msg)
 
-            return ws
-        return handler
-
-    def new_client(self, ws):
-        return Client(self, ws)
+        return ws
--- src/main.js
+++ src/main.js
@@ -1,9 +1,35 @@
 import React from 'react';
- 
-class Hello extends React.Component {
+
+class MessageList extends React.Component {
   render() {
     return <h1>Hello World</h1>
   }
 }
 
-React.render(<Hello/>, container);
+class App extends React.Component {
+  constructor() {
+    super();
+
+    // init websocket
+    this.ws = new WebSocket("wss://test.andrewstocker.net/ws/echo")
+    this.ws.onopen = () => {
+      this.ws.send("hello server")
+    }
+    this.ws.onmessage = this.handleMessage.bind(this)
+
+    // init state
+    this.state = {
+      data: {
+        messages: []
+      }
+    }
+  }
+  handleMessage(msg) {
+    console.log(msg);
+  }
+  render() {
+    return <MessageList messages={this.state.data.messages}/>
+  }
+}
+
+React.render(<App/>, container);
